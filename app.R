7f361e0f75b9ab8448dc52146687cde4aad6467a
James <jamesmaino@gmail.com>
2023-04-03 15:19:16 +1000

    remove draft and graph error

--- app.R
+++ app.R
@@ -5,43 +5,52 @@ library(DT)
 # k-means only works with numerical variables,
 # so don't give the user the option to select
 # a categorical variable
-d0 = readxl::read_xlsx("data/risk_index_matrix.xlsx", sheet=1, 
-                       col_types = rep("text", 11)) %>% 
+d0 <- readxl::read_xlsx("data/risk_index_matrix.xlsx",
+  sheet = 1,
+  col_types = rep("text", 11)
+) %>%
   mutate(Component = paste0(Component, " (", tolower(Attribute), ")")) %>%
-  pivot_longer(cols = c(OPTION_1,	OPTION_2,	OPTION_3, VALUE_1,	VALUE_2,	VALUE_3),
-               names_to ="vars", values_to="vals" ) %>% 
+  pivot_longer(
+    cols = c(OPTION_1, OPTION_2, OPTION_3, VALUE_1, VALUE_2, VALUE_3),
+    names_to = "vars", values_to = "vals"
+  ) %>%
   separate(vars, into = c("var", "no"), sep = "_") %>%
   select(-no) %>%
   pivot_wider(names_from = "var", values_from = "vals", values_fn = list) %>%
   unnest(c(OPTION, VALUE))
 
-d1 =  readxl::read_xlsx("data/risk_index_strategies.xlsx", sheet=1, 
-                        col_types = rep("text", 4))
+d1 <- readxl::read_xlsx("data/risk_index_strategies.xlsx",
+  sheet = 1,
+  col_types = rep("text", 4)
+)
 
-ggplot() + 
-  geom_point(aes(0,0), size=150, color="red") +
-  geom_text(aes(0,0, label="low")) +
+ggplot() +
+  geom_point(aes(0, 0), size = 150, color = "red") +
+  geom_text(aes(0, 0, label = "low")) +
   theme_void()
 
-getscore = function(d0, Crop, Num, Var){
-  out = d0 %>% 
+getscore <- function(d0, Crop, Num, Var) {
+  out <- d0 %>%
     filter(`Last year rotation` == Crop) %>%
     filter(Number == Num) %>%
-    filter(OPTION == Var) %>% 
-    pull(VALUE) %>% 
+    filter(OPTION == Var) %>%
+    pull(VALUE) %>%
     as.numeric()
-  ifelse(length(out)==0, 0, out)
+  ifelse(length(out) == 0, 0, out)
 }
 
-myselectize = function(labs, v, ind){
-  selectizeInput(inputId = paste0('x',ind),
-                 label = labs()[ind],
-                 choices = setNames(v()$vals[[ind]], v()$vars[[ind]]))
+myselectize <- function(labs, v, ind) {
+  selectizeInput(
+    inputId = paste0("x", ind),
+    label = labs()[ind],
+    choices = setNames(v()$vals[[ind]], v()$vars[[ind]])
+  )
 }
 
-ui <- fluidPage(theme = shinytheme("cerulean"),
+ui <- fluidPage(
+  theme = shinytheme("cerulean"),
   pageWithSidebar(
-    headerPanel('RLEM seasonal risk estimate (draft version)'),
+    headerPanel("RLEM severity risk assessment"),
     sidebarPanel(
       HTML("<i>Enter your paddock details below</i><br><br>"),
       selectInput("y0", "0. Last year's paddock", c("Crop", "Pasture")),
@@ -61,80 +70,103 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
   )
 )
 
-server <- 
-function(input, output, session) {
-  labs = reactive({
-    d0 %>% 
-      filter(`Last year rotation` == input$y0) %>%
-      distinct(Number, Component) %>%
-      mutate(lab = paste0(Number, ". ", Component)) %>%
-      arrange(lab) %>%
-      pull(lab) 
-  })
-  
-  v = reactive({
-    d0 %>% 
-      filter(`Last year rotation` == input$y0) %>%
-      group_by(Number, Component) %>%
-      summarise(vars = list(OPTION),
-                vals = list(VALUE),
-                .groups="drop") %>%
-      arrange(Number) 
-    
-  })
-  
-  output$y1 <- renderUI({ myselectize(labs, v, 1) })
-  output$y2 <- renderUI({ myselectize(labs, v, 2) })
-  output$y3 <- renderUI({ myselectize(labs, v, 3) })
-  output$y4 <- renderUI({ myselectize(labs, v, 4) })
-  output$y5 <- renderUI({ myselectize(labs, v, 5) })
-  output$y6 <- renderUI({ myselectize(labs, v, 6) })
-  output$y7 <- renderUI({ myselectize(labs, v, 7) })
-  output$y8 <- renderUI({if(input$y0 == "Pasture") myselectize(labs, v, 8) })
-  
-  risk <- reactive({
-    if(!is.null(input$x1)){
- 
-        as.numeric(input$x1) + 
-        as.numeric(input$x2) +
-        as.numeric(input$x3) +
-        as.numeric(input$x4) +
-        as.numeric(input$x5) +
-        as.numeric(input$x6) +
-        as.numeric(input$x7) +
-        ifelse(input$y0 == "Pasture", as.numeric(input$x8), 0)
-    }
+server <-
+  function(input, output, session) {
+    labs <- reactive({
+      d0 %>%
+        filter(`Last year rotation` == input$y0) %>%
+        distinct(Number, Component) %>%
+        mutate(lab = paste0(Number, ". ", Component)) %>%
+        arrange(lab) %>%
+        pull(lab)
+    })
 
-  })
-  
-  
-  riskrating <- reactive({
-    riskrating = "Low"
-    riskrating = ifelse(risk()>0, "Moderate", riskrating)
-    riskrating = ifelse(risk()>5, "High", riskrating)
-  })
-  
-  output$plot = renderPlot({
-    risktext = paste("Risk score =", risk(), "\n" , riskrating(), "risk")
-    riskcol = c(Low = "green", Moderate = "orange", High = "red")
-    ggplot() + 
-      geom_point(aes(0,0), size=150, color=riskcol[riskrating()], alpha=0.5) +
-      geom_text(aes(0,0, label=risktext), size=12) +
-      theme_void()
-    
-  })
-  
-  output$riskstrategies = renderTable({
-  
-    d1 %>% 
-      filter(Risk == riskrating()) %>% 
-      select(Period, Action) %>% 
-      mutate(Action = gsub('■', '<br>',Action))
-    
-    
+    v <- reactive({
+      d0 %>%
+        filter(`Last year rotation` == input$y0) %>%
+        group_by(Number, Component) %>%
+        summarise(
+          vars = list(OPTION),
+          vals = list(VALUE),
+          .groups = "drop"
+        ) %>%
+        arrange(Number)
+    })
 
-  }, sanitize.text.function=identity)
-  
-}
+    output$y1 <- renderUI({
+      myselectize(labs, v, 1)
+    })
+    output$y2 <- renderUI({
+      myselectize(labs, v, 2)
+    })
+    output$y3 <- renderUI({
+      myselectize(labs, v, 3)
+    })
+    output$y4 <- renderUI({
+      myselectize(labs, v, 4)
+    })
+    output$y5 <- renderUI({
+      myselectize(labs, v, 5)
+    })
+    output$y6 <- renderUI({
+      myselectize(labs, v, 6)
+    })
+    output$y7 <- renderUI({
+      myselectize(labs, v, 7)
+    })
+    output$y8 <- renderUI({
+      if (input$y0 == "Pasture") myselectize(labs, v, 8)
+    })
+
+    risk <- reactive({
+      if (!is.null(input$x1)) {
+        as.numeric(input$x1) +
+          as.numeric(input$x2) +
+          as.numeric(input$x3) +
+          as.numeric(input$x4) +
+          as.numeric(input$x5) +
+          as.numeric(input$x6) +
+          as.numeric(input$x7) +
+          ifelse(input$y0 == "Pasture", as.numeric(input$x8), 0)
+      }
+    })
+
+
+    riskrating <- reactive({
+      if (length(risk()) == 0) {
+        return(NULL)
+      }
+
+      riskrating <- "Low"
+      riskrating <- ifelse(risk() > 0, "Moderate", riskrating)
+      riskrating <- ifelse(risk() > 5, "High", riskrating)
+    })
+
+    output$plot <- renderPlot({
+      if (length(risk()) == 0 || length(riskrating()) == 0 || is.na(riskrating())) {
+        return(NULL)
+      }
+      risktext <- paste("Risk score =", risk(), "\n", riskrating(), "risk")
+      riskcol <- c(Low = "#49b649", Moderate = "#ffed2a", High = "#ff700a")
+
+      ggplot() +
+        geom_point(aes(0, 0), size = 150, color = riskcol[riskrating()], alpha = 0.5) +
+        geom_text(aes(0, 0), label = risktext, size = 12) +
+        theme_void()
+    })
+
+    output$riskstrategies <- renderTable(
+      {
+        if (length(riskrating()) == 0) {
+          return(NULL)
+        }
+        d1 %>%
+          filter(Risk == riskrating()) %>%
+          select(Period, Action) %>%
+          mutate(Action = gsub("■", "<br>", Action))
+      },
+      sanitize.text.function = identity
+    )
+  }
 
 shinyApp(ui, server)
